006b49e28ea4a25a26ae63c502e78dfd42877cf7
PantheraNestah <pantheranestah@gmail.com>
2024-05-28 23:05:16 +0300

    styled navbar and added legend sectin

--- React_Basics_v2/src/App.jsx
+++ React_Basics_v2/src/App.jsx
@@ -1,11 +1,15 @@
+import "./css/style.min.css"
+
 import MyHeader from './components/cHeader'
 import StackSection from './components/stack-section'
+import MyLegend from "./components/cLegend"
 
 function App() {
 
   return (
     <>
       <MyHeader />
+      <MyLegend />
       <StackSection />
     </>
   )
---
+++ React_Basics_v2/src/components/cLegend.jsx
@@ -0,0 +1,25 @@
+export default function MyLegend() {
+
+    return (
+        <>
+            <section className="col-12 d-flex align-items-center justify-content-between border mb-4">
+                <div className="legend_desc col-md-5 ps-1 d-flex flex-column align-items-start">
+                    <div>
+                        <h2>Gift Nestah P</h2>
+                        <span>Software Developer</span>
+                    </div>
+                    <p>
+                        I am a software developer with a passion for creating and developing software solutions, always looking for new challenges and opportunities to grow as a developer. I have a strong foundation in web development and a keen interest in Data and AI. Having a strong background in Computer Science, am a creative thinker always looking for new effective ways to solve problems and create innovative solutions. Am a self motivated individual with a deep value for collaboration and always looking to motivate and inspire my colleagues for the success of the whole team.
+                    </p>
+                    <div>
+                        <a href="#">My Resume</a>
+                        <a href="#">Connect</a>
+                    </div>
+                </div>
+                <div className="legend_img_cont">
+                    <img src="/img/github_rudrabarad.gif" width={"600px"} alt="Gift Nestah P" />
+                </div>
+            </section>
+        </>
+    )
+}
