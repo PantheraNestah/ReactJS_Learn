6fca459c84734d2365f1ca5ab05749234cb1a421
PantheraNestah <pantheranestah@gmail.com>
2024-06-03 10:04:20 +0300

    styled cLegend section

--- React_Basics_v2/src/components/cLegend.jsx
+++ React_Basics_v2/src/components/cLegend.jsx
@@ -2,8 +2,8 @@ export default function MyLegend() {
 
     return (
         <>
-            <section className="col-12 d-flex align-items-center justify-content-between border mb-4">
-                <div className="legend_desc col-md-5 ps-1 d-flex flex-column align-items-start">
+            <section className="col-12 d-flex align-items-center justify-content-between p-2 pb-4 mb-4">
+                <div className="legend_desc col-md-5 ps-1 d-flex flex-column align-items-start justify-content-between">
                     <div>
                         <h2>Gift Nestah P</h2>
                         <span>Software Developer</span>
@@ -11,9 +11,9 @@ export default function MyLegend() {
                     <p>
                         I am a software developer with a passion for creating and developing software solutions, always looking for new challenges and opportunities to grow as a developer. I have a strong foundation in web development and a keen interest in Data and AI. Having a strong background in Computer Science, am a creative thinker always looking for new effective ways to solve problems and create innovative solutions. Am a self motivated individual with a deep value for collaboration and always looking to motivate and inspire my colleagues for the success of the whole team.
                     </p>
-                    <div>
-                        <a href="#">My Resume</a>
-                        <a href="#">Connect</a>
+                    <div className="cv_connect d-flex justify-content-between">
+                        <a href="#" className="bg-primary text-center me-2">My Resume</a>
+                        <a href="#" className="text-center ms-2">Connect</a>
                     </div>
                 </div>
                 <div className="legend_img_cont">
--- React_Basics_v2/src/components/stack-section.jsx
+++ React_Basics_v2/src/components/stack-section.jsx
@@ -4,10 +4,10 @@ export default function StackSection() {
     
         return (
             <>
-                <section className="sect_stack col-10 d-flex justify-content-center">
+                <section className="sect_stack col-12 d-flex justify-content-center">
                     <div className="sect_stack_container col-8 mx-auto">
                         <div className="center-line"></div>
-                        <ul className="stack_list border d-flex flex-column align-items-center">
+                        <ul className="stack_list d-flex flex-column align-items-center">
                             <li className="item col-lg-2 text-center">HTML & CSS</li>
                             <li className="item col-lg-2 text-center">JavaScript</li>
                             <li className="item col-lg-2 text-center">React JS</li>
