76ba026ad07a5253c09f1207be41becb6263348a
PantheraNestah <pantheranestah@gmail.com>
2024-06-06 22:47:38 +0300

    styled and created landing page whith nav and hero components

---
+++ real_esto/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+import Nav from './components/Nav/Nav';
+import Hero from './components/Hero/Hero';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
---
+++ real_esto/src/components/Nav/Nav.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./Nav.scss";
+
+const Nav = () => {
+    return (
+        <nav className="app_nav d-flex align-items-end">
+            <h1>Real Homez</h1>
+            <ul className="nav_links col-lg-7 d-flex align-items-end justify-content-center">
+                <li><a className="nav_link" href="#">Home</a></li>
+                <li><a className="nav_link" href="#">Properties</a></li>
+                <li><a className="nav_link" href="#">Agents</a></li>
+                <li><a className="nav_link" href="#">About</a></li>
+                <li><a className="nav_link" href="#">Contact</a></li>
+            </ul>
+            <ul className="reg_links d-flex align-items-end">
+                <li><a href="#">Sign In</a></li>|
+                <li><a href="#">Register</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
