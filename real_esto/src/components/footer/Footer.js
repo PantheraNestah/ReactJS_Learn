85b62a4e4dfb832875f61b1cec0182e148991193
PantheraNestah <pantheranestah@gmail.com>
2024-06-06 23:01:22 +0300

    added footer

---
+++ real_esto/src/components/footer/Footer.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+    <footer>
+        <div className="footer_cont">
+            <div></div>
+            <div>
+                <div>
+                    <h5>Get in touch with us</h5>
+                    <p>Real Homez</p>
+                    <p>123, Main Street, New York, USA</p>
+                    <p>Phone: +1234567890</p>
+                    <p>Email: real.homez@info.co.ke</p>
+                    <p>Whatsapp: qrcode</p>
+                </div>
+                <div>
+                    <h5>Our Socials</h5>
+                    <div>
+                        <a href="#">Facebook</a>
+                        <a href="#">Twitter</a>
+                        <a href="#">Instagram</a>
+                        <a href="#">LinkedIn</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+}
