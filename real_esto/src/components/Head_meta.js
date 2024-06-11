6cd0dc2d4a0f6cb1901ec928ae1dcb4efe1d9006
PantheraNestah <pantheranestah@gmail.com>
2024-06-11 23:00:53 +0300

    adding properties swiper

---
+++ real_esto/src/components/Head_meta.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Head_meta = () => {
+    return (
+        <head>
+            <meta charset="utf-8" />
+            <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+            <meta content="" name="keywords" />
+            <meta content="" name="description" />    
+            {/* Icon Font Stylesheet */}
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" />
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+        </head>
+    )
+}
+
+export default Head_meta;
